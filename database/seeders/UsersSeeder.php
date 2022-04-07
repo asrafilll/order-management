9a60801bb6685303c4b99d56232ead479aaee173
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-07 08:19:18 +0700

    wip: add users seeder for default account

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate([
+            'email' => 'superadmin@example.com',
+        ], [
+            'name' => 'Super Admin',
+            'password' => Hash::make('secret'),
+            'email_verified_at' => Carbon::now(),
+        ]);
+    }
+}
