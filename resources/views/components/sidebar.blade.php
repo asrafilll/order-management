35c757cd250d90a1b2ef0d8d09a41266fbde4505
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-09 16:45:17 +0700

    feat: change user image on sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -19,9 +19,9 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img
-                    src="{{ url('/themes') }}/img/user2-160x160.jpg"
-                    class="img-circle elevation-2"
-                    alt="User Image"
+                    src="{{ url('/themes') }}/img/user-160x160.jpg"
+                    class="img-circle"
+                    alt="{{ Auth::user()->name }}"
                 >
             </div>
             <div class="info">
