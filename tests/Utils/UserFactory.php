b0c878141798bf76c98597bd3a85b8e7be9286e1
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-09 16:18:16 +0700

    add: user factory tests

---
+++ tests/Utils/UserFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Utils;
+
+use App\Enums\PermissionEnum;
+use App\Models\User;
+use Database\Seeders\PermissionSeeder;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+trait UserFactory
+{
+    public function createUserWithPermission(PermissionEnum $permissionEnum): User|Authenticatable
+    {
+        $this->seed(PermissionSeeder::class);
+
+        /** @var User */
+        $user = User::factory()->create();
+        $user->givePermissionTo($permissionEnum->value);
+
+        return $user;
+    }
+}
