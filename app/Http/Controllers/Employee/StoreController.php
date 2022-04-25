7cf73417256edaeb2caa8d8536c1b1522ccd1bb1
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-25 13:26:51 +0700

    wip: store and update employee feature

--- app/Http/Controllers/Employee/StoreController.php
+++ app/Http/Controllers/Employee/StoreController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers\Employee;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Employee\StoreRequest;
+use App\Models\Employee;
+use Illuminate\Support\Facades\Response;
 
 class StoreController extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param StoreRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(StoreRequest $storeRequest)
     {
-        //
+        $employee = Employee::create($storeRequest->validated());
+        $message = __('crud.created', [
+            'name' => 'employee',
+        ]);
+
+        return Response::redirectToRoute('employees.edit', $employee)
+            ->with('success', $message);
     }
 }
--- app/Http/Controllers/Employee/UpdateController.php
+++ app/Http/Controllers/Employee/UpdateController.php
@@ -3,18 +3,25 @@
 namespace App\Http\Controllers\Employee;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Employee\UpdateRequest;
+use App\Models\Employee;
+use Illuminate\Support\Facades\Response;
 
 class UpdateController extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Employee $employee
+     * @param UpdateRequest $updateRequest
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Employee $employee, UpdateRequest $updateRequest)
     {
-        //
+        $employee->update($updateRequest->validated());
+        $message = __('crud.updated', [
+            'name' => 'employee',
+        ]);
+
+        return Response::redirectToRoute('employees.edit', $employee)
+            ->with('success', $message);
     }
 }
