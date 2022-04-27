dda82915655551d1fc3a8139aae295afefdd171d
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-27 10:24:22 +0700

    wip(orders): add order status draft and set nullable order fields

--- database/migrations/2022_04_26_232526_create_orders_table.php
+++ database/migrations/2022_04_26_232526_create_orders_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Enums\OrderStatusEnum;
-use App\Enums\PaymentStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,7 +20,7 @@ class CreateOrdersTable extends Migration
             $table
                 ->string('status')
                 ->index()
-                ->default(OrderStatusEnum::waiting()->value);
+                ->default(OrderStatusEnum::draft()->value);
             $table->unsignedBigInteger('source_id');
             $table->string('source_name');
             $table->unsignedBigInteger('customer_id');
@@ -33,22 +32,22 @@ class CreateOrdersTable extends Migration
             $table->string('customer_subdistrict');
             $table->string('customer_village');
             $table->string('customer_postal_code');
-            $table->unsignedBigInteger('payment_method_id');
-            $table->string('payment_method_name');
+            $table->unsignedBigInteger('payment_method_id')->nullable();
+            $table->string('payment_method_name')->nullable();
             $table
                 ->string('payment_status')
                 ->index()
-                ->default(PaymentStatusEnum::unpaid()->value);
-            $table->unsignedBigInteger('shipping_id');
-            $table->string('shipping_name');
+                ->nullable();
+            $table->unsignedBigInteger('shipping_id')->nullable();
+            $table->string('shipping_name')->nullable();
             $table->date('shipping_date')->nullable();
             $table->string('shipping_airwaybill')->nullable();
-            $table->unsignedBigInteger('items_quantity');
-            $table->unsignedBigInteger('items_price');
+            $table->unsignedBigInteger('items_quantity')->nullable();
+            $table->unsignedBigInteger('items_price')->nullable();
             $table->unsignedBigInteger('items_discount')->nullable();
-            $table->unsignedBigInteger('shipping_price');
+            $table->unsignedBigInteger('shipping_price')->nullable();
             $table->unsignedBigInteger('shipping_discount')->nullable();
-            $table->unsignedBigInteger('total_price');
+            $table->unsignedBigInteger('total_price')->nullable();
             $table->string('note')->nullable();
             $table->timestamp('returned_at')->nullable();
             $table->string('returned_note')->nullable();
