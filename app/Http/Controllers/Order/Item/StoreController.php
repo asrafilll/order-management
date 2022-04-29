6e68d01d12a7b3803799d134814a71dad5865ae7
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-29 16:19:27 +0700

    wip(orders): increase order item quantity when add product which already already exists in order items

--- app/Http/Controllers/Order/Item/StoreController.php
+++ app/Http/Controllers/Order/Item/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Order\Item;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Order\Item\StoreRequest;
 use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\ProductVariant;
 use Illuminate\Support\Facades\Response;
 
@@ -20,22 +21,33 @@ class StoreController extends Controller
         /** @var ProductVariant */
         $productVariant = ProductVariant::find($storeRequest->get('variant_id'));
         $productVariant->load(['product']);
-        $order
+        /** @var OrderItem */
+        $orderItem = $order
             ->items()
-            ->create([
-                'product_id' => $productVariant->product_id,
-                'product_slug' => $productVariant->product->slug,
-                'product_name' => $productVariant->product->name,
-                'product_description' => $productVariant->product->description,
-                'variant_id' => $productVariant->id,
-                'variant_name' => $productVariant->name,
-                'variant_price' => $productVariant->price,
-                'variant_weight' => $productVariant->weight,
-                'variant_option1' => $productVariant->option1,
-                'variant_value1' => $productVariant->value1,
-                'variant_option2' => $productVariant->option2,
-                'variant_value2' => $productVariant->value2,
-            ]);
+            ->where('variant_id', $storeRequest->get('variant_id'))
+            ->first();
+
+        if ($orderItem) {
+            $orderItem->increment('quantity');
+        } else {
+            $order
+                ->items()
+                ->create([
+                    'product_id' => $productVariant->product_id,
+                    'product_slug' => $productVariant->product->slug,
+                    'product_name' => $productVariant->product->name,
+                    'product_description' => $productVariant->product->description,
+                    'variant_id' => $productVariant->id,
+                    'variant_name' => $productVariant->name,
+                    'variant_price' => $productVariant->price,
+                    'variant_weight' => $productVariant->weight,
+                    'variant_option1' => $productVariant->option1,
+                    'variant_value1' => $productVariant->value1,
+                    'variant_option2' => $productVariant->option2,
+                    'variant_value2' => $productVariant->value2,
+                ]);
+        }
+
 
         $message = __('crud.created', [
             'name' => 'order item',
--- tests/Feature/Order/Item/CreateOrderItemTest.php
+++ tests/Feature/Order/Item/CreateOrderItemTest.php
@@ -245,4 +245,122 @@ class CreateOrderItemTest extends TestCase
                 'variant_id',
             ]);
     }
+
+    /**
+     * @return void
+     */
+    public function test_should_not_create_new_order_item_when_order_item_already_exists()
+    {
+        /** @var Product */
+        $product = Product::factory()->create();
+        $product
+            ->options()
+            ->create([
+                'name' => 'Color',
+                'values' => json_encode([
+                    'Red',
+                    'Green',
+                    'Blue',
+                ])
+            ]);
+        $product
+            ->variants()
+            ->createMany([
+                [
+                    'name' => 'Red',
+                    'price' => 10000,
+                    'weight' => 1000,
+                    'option1' => 'Color',
+                    'value1' => 'Red',
+                ],
+                [
+                    'name' => 'Green',
+                    'price' => 11000,
+                    'weight' => 1100,
+                    'option1' => 'Color',
+                    'value1' => 'Green',
+                ],
+                [
+                    'name' => 'Blue',
+                    'price' => 11000,
+                    'weight' => 1100,
+                    'option1' => 'Color',
+                    'value1' => 'Blue',
+                ],
+            ]);
+
+        /** @var ProductVariant */
+        $productVariant = ProductVariant::with(['product'])
+            ->inRandomOrder()
+            ->first();
+        /** @var OrderSource */
+        $orderSource = OrderSource::factory()->create();
+        /** @var Customer */
+        $customer = Customer::factory()->create();
+        /** @var Order */
+        $order = Order::create([
+            'source_id' => $orderSource->id,
+            'source_name' => $orderSource->name,
+            'customer_id' => $customer->id,
+            'customer_name' => $customer->name,
+            'customer_phone' => $customer->phone,
+            'customer_address' => $customer->address,
+            'customer_province' => $customer->province,
+            'customer_city' => $customer->city,
+            'customer_subdistrict' => $customer->subdistrict,
+            'customer_village' => $customer->village,
+            'customer_postal_code' => $customer->postal_code,
+        ]);
+        /** @var OrderItem */
+        $orderItem = $order
+            ->items()
+            ->create([
+                'product_id' => $productVariant->product_id,
+                'product_slug' => $productVariant->product->slug,
+                'product_name' => $productVariant->product->name,
+                'product_description' => $productVariant->product->description,
+                'variant_id' => $productVariant->id,
+                'variant_name' => $productVariant->name,
+                'variant_price' => $productVariant->price,
+                'variant_weight' => $productVariant->weight,
+                'variant_option1' => $productVariant->option1,
+                'variant_value1' => $productVariant->value1,
+                'variant_option2' => $productVariant->option2,
+                'variant_value2' => $productVariant->value2,
+                'quantity' => 1,
+            ]);
+
+        $input = [
+            'variant_id' => $productVariant->id,
+        ];
+
+        $response = $this
+            ->actingAs(
+                $this->createUserWithPermission(
+                    PermissionEnum::manage_orders()
+                )
+            )
+            ->post(route('orders.items.store', $order), $input);
+
+        $response
+            ->assertRedirect()
+            ->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas((new OrderItem()), [
+            'id' => $orderItem->id,
+            'product_id' => $productVariant->product_id,
+            'product_slug' => $productVariant->product->slug,
+            'product_name' => $productVariant->product->name,
+            'product_description' => $productVariant->product->description,
+            'variant_id' => $productVariant->id,
+            'variant_name' => $productVariant->name,
+            'variant_price' => $productVariant->price,
+            'variant_weight' => $productVariant->weight,
+            'variant_option1' => $productVariant->option1,
+            'variant_value1' => $productVariant->value1,
+            'variant_option2' => $productVariant->option2,
+            'variant_value2' => $productVariant->value2,
+            'quantity' => 2, // +1 because already exists
+        ]);
+    }
 }
