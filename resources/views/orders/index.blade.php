704a11b61a280c870b3faf86d0bae809323ed400
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-04-30 16:43:35 +0700

    wip(orders): add filter and export button (not feature)

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -14,29 +14,42 @@
     <section class="content">
         <div class="card">
             <div class="card-header">
-                <form
-                    action=""
-                    metho="GET"
-                >
-                    <div class="input-group">
-                        <input
-                            type="text"
-                            name="search"
-                            class="form-control"
-                            placeholder="Search here"
-                            value="{{ Request::get('search') }}"
-                        >
-
-                        <div class="input-group-append">
-                            <button
-                                type="submit"
-                                class="btn btn-default"
+                <div class="row align-items-center">
+                    <form
+                        action=""
+                        method="GET"
+                        class="col-lg mb-2 mb-lg-0"
+                    >
+                        <div class="input-group">
+                            <input
+                                type="text"
+                                name="search"
+                                class="form-control"
+                                placeholder="Search here"
+                                value="{{ Request::get('search') }}"
                             >
-                                <i class="fas fa-search"></i>
-                            </button>
+
+                            <div class="input-group-append">
+                                <button
+                                    type="submit"
+                                    class="btn btn-default"
+                                >
+                                    <i class="fas fa-search"></i>
+                                </button>
+                            </div>
                         </div>
+                    </form>
+                    <div class="col-lg-auto">
+                        <button
+                            type="button"
+                            class="btn btn-default"
+                        >{{ __('Filter') }}</button>
+                        <button
+                            type="button"
+                            class="btn btn-default"
+                        >{{ __('Export') }}</button>
                     </div>
-                </form>
+                </div>
             </div>
             <div class="card-body table-responsive p-0">
                 <table class="table table-hover text-nowrap">
