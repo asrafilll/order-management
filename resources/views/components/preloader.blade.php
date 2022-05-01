a302641a91e2c1f62b1859d0746866a38a0e36e1
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-05-01 09:04:35 +0700

    fix: change logo and preloader image

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -1,9 +1,7 @@
 <div class="preloader flex-column justify-content-center align-items-center">
     <img
         class="animation__shake"
-        src="{{ url('/themes') }}/img/logo.png"
+        src="{{ url('/themes') }}/img/logo-150x150.png"
         alt="{{ Config::get('app.name') }}"
-        height="60"
-        width="60"
     >
 </div>
