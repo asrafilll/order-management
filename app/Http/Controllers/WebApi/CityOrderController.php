c4decf028091926b3fdf66aaafa1cb46a6e17b05
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-06-18 20:23:42 +0700

    feat(dashboard): add filter on chart order by cities

--- app/Http/Controllers/WebApi/CityOrderController.php
+++ app/Http/Controllers/WebApi/CityOrderController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers\WebApi;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CityOrderResource;
 use App\Models\Order;
+use Illuminate\Http\Request;
 
 class CityOrderController extends Controller
 {
     /**
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         return CityOrderResource::collection(
             Order::query()
                 ->selectRaw('customer_city as city, COUNT(id) as total')
+                ->when($request->filled('start_date'), function ($query) use ($request) {
+                    $query->whereRaw('DATE(created_at) >= ?', [$request->get('start_date')]);
+                })
+                ->when($request->filled('end_date'), function ($query) use ($request) {
+                    $query->whereRaw('DATE(created_at) <= ?', [$request->get('end_date')]);
+                })
                 ->groupBy('customer_city')
                 ->get()
         );
