52c15662193871c6c1a84569888a3da023ef9b6c
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-06-18 20:41:10 +0700

    feat(dashboard): add date filter on chart customer by types

--- app/Http/Controllers/WebApi/CustomerTypeCustomerController.php
+++ app/Http/Controllers/WebApi/CustomerTypeCustomerController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers\WebApi;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CustomerTypeCustomerResource;
 use App\Models\Customer;
+use Illuminate\Http\Request;
 
 class CustomerTypeCustomerController extends Controller
 {
     /**
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         return CustomerTypeCustomerResource::collection(
             Customer::query()
                 ->selectRaw('UPPER(type) as name, COUNT(id) as total_customers')
+                ->when($request->filled('start_date'), function ($query) use ($request) {
+                    $query->whereRaw('DATE(created_at) >= ?', [$request->get('start_date')]);
+                })
+                ->when($request->filled('end_date'), function ($query) use ($request) {
+                    $query->whereRaw('DATE(created_at) <= ?', [$request->get('end_date')]);
+                })
                 ->groupBy('type')
                 ->get()
         );
--- resources/views/components/chart-customer-by-types.blade.php
+++ resources/views/components/chart-customer-by-types.blade.php
@@ -1,17 +1,27 @@
 <div>
-    <div
-        class="card"
-        id="customer-by-types-module"
-    >
-        <div class="card-header">
-            <h3 class="card-title">{{ __('Customer By Types') }}</h3>
+    <div id="customer-by-types-module">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">{{ __('Customer By Types') }}</h3>
+                <div class="card-tools">
+                    <button
+                        type="button"
+                        class="btn btn-default"
+                        data-toggle="modal"
+                        data-target="#filter-CustomerByTypes"
+                    >
+                        <i class="fas fa-calendar-alt"></i>
+                    </button>
+                </div>
+            </div>
+            <div
+                class="card-body"
+                style="position: relative: height: 350px;"
+            >
+                <canvas id="customer-by-types-chart"></canvas>
+            </div>
         </div>
-        <div
-            class="card-body"
-            style="position: relative: height: 350px;"
-        >
-            <canvas id="customer-by-types-chart"></canvas>
-        </div>
+        <x-chart-date-filter-modal name="CustomerByTypes" />
     </div>
     <script>
         const CustomerByTypesModule = (function() {
@@ -19,10 +29,14 @@
             const $chart = document.getElementById('customer-by-types-chart');
             const ctx = $chart.getContext('2d');
 
+            const baseUrl = '{{ route('web-api.customer-types.customers.index') }}';
+            let chart;
+            let url = baseUrl;
+
             init();
 
             function init() {
-                const chart = new Chart(ctx, {
+                chart = new Chart(ctx, {
                     type: 'pie',
                     options: {
                         legend: {
@@ -31,8 +45,12 @@
                     },
                 });
 
+                render();
+            }
+
+            function render() {
                 $.ajax({
-                    url: '{{ route('web-api.customer-types.customers.index') }}',
+                    url: url,
                     success: function(response) {
                         chart.data = {
                             labels: response.data.map(value => value.name),
@@ -46,6 +64,16 @@
                     },
                 });
             }
+
+            function setParams(params) {
+                url = baseUrl + '?' + params;
+
+                render();
+            }
+
+            return {
+                setParams: setParams,
+            };
         })();
     </script>
 </div>
