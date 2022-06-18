c6d03d43a951496746186c1388e3c0d4bbd1a5b6
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-06-18 20:20:19 +0700

    feat(dashboard): add filter on chart order by statuses

--- app/Http/Controllers/WebApi/OrderStatusOrderController.php
+++ app/Http/Controllers/WebApi/OrderStatusOrderController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers\WebApi;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderStatusOrderResource;
 use App\Models\Order;
-use Illuminate\Support\Carbon;
+use Illuminate\Http\Request;
 
 class OrderStatusOrderController extends Controller
 {
     /**
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         return OrderStatusOrderResource::collection(
             Order::query()
                 ->selectRaw('UPPER(status) as name, COUNT(id) as total_orders')
+                ->when($request->filled('start_date'), function ($query) use ($request) {
+                    $query->whereRaw('DATE(created_at) >= ?', [$request->get('start_date')]);
+                })
+                ->when($request->filled('end_date'), function ($query) use ($request) {
+                    $query->whereRaw('DATE(created_at) <= ?', [$request->get('end_date')]);
+                })
                 ->groupBy('status')
                 ->get()
         );
--- resources/views/components/chart-order-by-statuses.blade.php
+++ resources/views/components/chart-order-by-statuses.blade.php
@@ -1,28 +1,43 @@
 <div>
-    <div
-        class="card"
-        id="order-by-statuses-module"
-    >
-        <div class="card-header">
-            <h3 class="card-title">{{ __('Order By Statuses') }}</h3>
+    <div id="order-by-statuses-module">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">{{ __('Order By Statuses') }}</h3>
+                <div class="card-tools">
+                    <button
+                        type="button"
+                        class="btn btn-default"
+                        data-toggle="modal"
+                        data-target="#filter-OrderByStatuses"
+                    >
+                        <i class="fas fa-calendar-alt"></i>
+                    </button>
+                </div>
+            </div>
+            <div
+                class="card-body"
+                style="position: relative: height: 350px;"
+            >
+                <canvas id="order-by-statuses-chart"></canvas>
+            </div>
         </div>
-        <div
-            class="card-body"
-            style="position: relative: height: 350px;"
-        >
-            <canvas id="order-by-statuses-chart"></canvas>
-        </div>
+        <x-chart-date-filter-modal name="OrderByStatuses" />
     </div>
+
     <script>
         const OrderByStatusesModule = (function() {
             const $el = $('#order-by-statuses-module');
             const $chart = document.getElementById('order-by-statuses-chart');
             const ctx = $chart.getContext('2d');
 
+            const baseUrl = '{{ route('web-api.order-statuses.orders.index') }}';
+            let chart;
+            let url = baseUrl;
+
             init();
 
             function init() {
-                const chart = new Chart(ctx, {
+                chart = new Chart(ctx, {
                     type: 'pie',
                     options: {
                         legend: {
@@ -31,8 +46,12 @@
                     },
                 });
 
+                render();
+            }
+
+            function render() {
                 $.ajax({
-                    url: '{{ route('web-api.order-statuses.orders.index') }}',
+                    url: url,
                     success: function(response) {
                         chart.data = {
                             labels: response.data.map(value => value.name),
@@ -46,6 +65,16 @@
                     },
                 });
             }
+
+            function setParams(params) {
+                url = baseUrl + '?' + params;
+
+                render();
+            }
+
+            return {
+                setParams: setParams,
+            };
         })();
     </script>
 </div>
